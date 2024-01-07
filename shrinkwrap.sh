46cb5fafb0e72aaf643f7da2e56bbe47b0c7e62b
Suvir Kumar <suvir@talkkonnect.com>
2024-01-07 12:44:29 +0700

    Update shrinkwrap.sh

--- shrinkwrap.sh
+++ shrinkwrap.sh
@@ -8,27 +8,27 @@ if [ $# -eq 0 ]
     exit 1
 fi
 set -e
-sudo fdisk -l $1
-sudo fdisk -l $1 > /tmp/fdisk.log
+ fdisk -l $1
+ fdisk -l $1 > /tmp/fdisk.log
 
 START=$(cat /tmp/fdisk.log | grep "83 Linux" | awk '{print $2}')
 
 echo "START of partition: $START"
 
-sudo losetup -d /dev/loop0 || echo "Good - no /dev/loop0 is already free"
-sudo losetup /dev/loop0 $1
-sudo partprobe /dev/loop0
-sudo lsblk /dev/loop0
-sudo e2fsck -f /dev/loop0p2
-sudo resize2fs -p /dev/loop0p2 -M
-sudo dumpe2fs -h /dev/loop0p2 | tee /tmp/dumpe2fs
+ losetup -d /dev/loop0 || echo "Good - no /dev/loop0 is already free"
+ losetup /dev/loop0 $1
+ partprobe /dev/loop0
+ lsblk /dev/loop0
+ e2fsck -f /dev/loop0p2
+ resize2fs -p /dev/loop0p2 -M
+ dumpe2fs -h /dev/loop0p2 | tee /tmp/dumpe2fs
 # Calculate the size of the resized filesystem in 512 blocks which we'll need
 # later for fdisk to also resize the partition add 16 blocks just to be safe
 NEWSIZE=$(cat /tmp/dumpe2fs |& awk -F: '/Block count/{count=$2} /Block size/{size=$2} END{print count*size/512 +  16}')
 echo "NEW SIZE of partition: $NEWSIZE  512-blocks"
 
 # now pipe commands to fdisk
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk /dev/loop0 || echo "Ignore that error."
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF |  fdisk /dev/loop0 || echo "Ignore that error."
   p # print the in-memory partition table
   d # delete partition
   2 # partition 2
@@ -43,29 +43,29 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk /dev/loop0 || echo "I
   q # and we're done
 EOF
 
-sudo fdisk -l $1
-sudo fdisk -l $1 > /tmp/fdisk_new.log
-sudo losetup -d /dev/loop0
+ fdisk -l $1
+ fdisk -l $1 > /tmp/fdisk_new.log
+ losetup -d /dev/loop0
 
-FINALEND_BYTES=$(cat /tmp/fdisk_new.log | grep "83 Linux" | awk '{print ($3+1)*512}')
+FINALEND_BYTES=$(cat /tmp/fdisk_new.log | grep "83 Linux" | awk '{printf "%.0f", ($3+1)*512}')
 echo "TRUNCATE AT: $FINALEND_BYTES bytes"
 
 # Truncate the image file on disk
-sudo truncate -s $FINALEND_BYTES $1
+ truncate -s $FINALEND_BYTES $1
 
 # Fill the empty space with zeros for better compressability
-sudo losetup /dev/loop0 $1
-sudo partprobe /dev/loop0
-sudo mkdir -p /tmp/mountpoint
-sudo mount /dev/loop0p2 /tmp/mountpoint
-sudo dd if=/dev/zero of=/tmp/mountpoint/zero.txt  status=progress || echo "Expected to fail with out of space"
-sudo rm /tmp/mountpoint/zero.txt
+ losetup /dev/loop0 $1
+ partprobe /dev/loop0
+ mkdir -p /tmp/mountpoint
+ mount /dev/loop0p2 /tmp/mountpoint
+ dd if=/dev/zero of=/tmp/mountpoint/zero.txt  status=progress || echo "Expected to fail with out of space"
+ rm /tmp/mountpoint/zero.txt
 df -h /tmp/mountpoint
-sudo umount /tmp/mountpoint
+ umount /tmp/mountpoint
 lsblk
-sudo rmdir /tmp/mountpoint
+ rmdir /tmp/mountpoint
 
 echo "We're done. Final info: "
-sudo fdisk -l $1
-sudo dumpe2fs -h /dev/loop0p2 | tee /tmp/dumpe2fs
-sudo losetup -d /dev/loop0
+ fdisk -l $1
+ dumpe2fs -h /dev/loop0p2 | tee /tmp/dumpe2fs
+ losetup -d /dev/loop0
